cd385cfbdfe7e6b2b0473e51dce029f682e7640c
Kate McFaul <katemcfaul@gmail.com>
2017-11-24 17:28:27 -0800

    Add playlist structure

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Playlist.css';
+import { TrackList } from '../TrackList/TrackList';
+
+export class Playlist extends Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={'New Playlist'}/>
+        //<TrackList />
+        <a class="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+    );
+  }
+}
