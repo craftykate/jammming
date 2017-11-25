5b5efd5f240a83e3a803500796ffce709b8be5cc
Kate McFaul <katemcfaul@gmail.com>
2017-11-24 18:59:09 -0800

    formatting

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,7 @@ export class Playlist extends Component {
   render() {
     return (
       <div className="Playlist">
-          <input defaultValue={'New Playlist'} />
+        <input defaultValue={'New Playlist'} />
         <TrackList tracks={this.props.playlistTracks} />
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
